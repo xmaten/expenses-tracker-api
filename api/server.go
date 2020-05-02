71a91d0ef3a663eb44893b6ab825189974eba359
Mateusz Wójcik <mateusz.wojcik@start-up.house>
2020-05-02 21:00:18 +0200

    Create entry point

---
+++ api/server.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/xmaten/expenses-tracker-api/api/controllers"
+	"github.com/xmaten/expenses-tracker-api/api/seed"
+	"log"
+	"os"
+)
+
+var server = controllers.Server{}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("sad .env file found")
+	}
+}
+
+func Run() {
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error getting env, %v", err)
+	} else {
+		fmt.Println("We are getting values")
+	}
+
+	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+
+	seed.Load(server.DB)
+
+	apiPort := fmt.Sprintf(":%s", os.Getenv("API_PORT"))
+	fmt.Printf("Listening to port %s", apiPort)
+
+	server.Run(apiPort)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/xmaten/expenses-tracker-api/api"
+
+func main() {
+	api.Run()
+}
