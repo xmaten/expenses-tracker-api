8422d5e2d6688b171f7ae0183f2db390f18b466d
Mateusz Wójcik <mateusz.wojcik@start-up.house>
2020-05-02 12:17:20 +0200

    Init projects

---
+++ api/models/User.go
@@ -0,0 +1,90 @@
+package models
+
+import (
+	"errors"
+	"github.com/badoux/checkmail"
+	"html"
+	"strings"
+	"time"
+)
+
+type User struct {
+	ID        uint32    `gorm:"primary_key;auto_increment" json:"id"`
+	Username  string    `gorm:"size:255;not null;unique" json:"username"`
+	Email     string    `gorm:"size:100;not null;unique" json:"email"`
+	Password  string    `gorm:"size:100;not null;" json:"password"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+func (u *User) BeforeSave() (err error)  {
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) Prepare() {
+	u.Username = html.EscapeString(strings.TrimSpace(u.Username))
+	u.Email = html.EscapeString(strings.TrimSpace(u.Email))
+	u.CreatedAt = time.Now()
+	u.UpdatedAt = time.Now()
+}
+
+func (u *User) AfterFind() (err error)  {
+	if  err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *User) Validate(action string) map[string]string {
+	var errorMessages = make(map[string]string)
+	var err error
+
+	switch strings.ToLower(action) {
+	case "update":
+		if u.Email == "" {
+			err = errors.New("required email")
+			errorMessages["Required_email"] = err.Error()
+		}
+		if u.Email != "" {
+			if err = checkmail.ValidateFormat(u.Email); err != nil {
+				err = errors.New("invalid email")
+				errorMessages["Invalid_email"] = err.Error()
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/xmaten/expenses-tracker-api
+
+go 1.14
+
+require (
+	github.com/badoux/checkmail v0.0.0-20181210160741-9661bd69e9ad // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-contrib/cors v1.3.1 // indirect
+	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607 // indirect
+	github.com/gin-gonic/gin v1.6.2 // indirect
+	github.com/jinzhu/gorm v1.9.12 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	github.com/twinj/uuid v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+)
